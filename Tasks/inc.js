43c5f43ce822e9f73c887c1a39d3649f2fe0f1ca
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-01-13 20:19:56 +0200

    Add const/let, rename function/variables

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -2,13 +2,13 @@
 
 "use strict";
 
-let inc_numbers = (format_complete, ...rest_variables) => {
-  for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === "N") {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+const incNumbers = (dict, ...rest_variables) => {
+  for (const key in dict) {
+    if ((typeof dict[key]).charAt(0).toUpperCase() === "N") {
+      dict[key] = dict[key] + 1;
     }
   }
-  return format_complete;
+  return dict;
 };
 
-module.exports = inc_numbers;
+module.exports = incNumbers;

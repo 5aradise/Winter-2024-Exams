5596f744afaf0faa9bb1032ff2b87ac5b9c41c66
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-01-14 00:31:13 +0200

    Rename variables to camelCase

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,10 +4,10 @@
 
 const intersection = (obj1, obj2) => {
   const intersec = {};
-  const obj1_Keys = Object.keys(obj1);
-  for (const obj1_Key of obj1_Keys) {
-    if (obj1[obj1_Key] === obj2[obj1_Key]) {
-      intersec[obj1_Key] = obj1[obj1_Key];
+  const obj1Keys = Object.keys(obj1);
+  for (const obj1Key of obj1Keys) {
+    if (obj1[obj1Key] === obj2[obj1Key]) {
+      intersec[obj1Key] = obj1[obj1Key];
     }
   }
   return intersec;

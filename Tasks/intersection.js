86460c3d7851fe993c652ae85a73c570384c4e47
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-01-13 21:31:05 +0200

    Remove senseless expression, do not mutate function input parameter

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,15 +3,14 @@
 "use strict";
 
 const intersection = (obj1, obj2) => {
+  const intersec = {};
   const obj1_Keys = Object.keys(obj1);
   for (const obj1_Key of obj1_Keys) {
     if (obj1[obj1_Key] === obj2[obj1_Key]) {
-      obj2[obj1_Key] = obj1[obj1_Key];
-    } else {
-      delete obj1[obj1_Key];
+      intersec[obj1_Key] = obj1[obj1_Key];
     }
   }
-  return obj1;
+  return intersec;
 };
 
 module.exports = intersection;

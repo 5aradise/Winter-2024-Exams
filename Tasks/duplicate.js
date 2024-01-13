b9c24fa348a813da771d16cc894e04ca7fb0c44e
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-01-13 20:12:18 +0200

    Delete senseless case, replace expression with .push()

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,11 @@
 "use strict";
 
 const duplicate = (value, times) => {
-  if (times <= 0) return [];
-  else {
-    const result = [];
-    for (let i = 0; i < times; i++) {
-      result[i] = value;
-    }
-    return result;
+  const result = [];
+  for (let i = 0; i < times; i++) {
+    result.push(value);
   }
+  return result;
 };
 
 module.exports = duplicate;
